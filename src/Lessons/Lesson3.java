333de2f2827b37f36a06f22bf44854095acc5fbf
Mikhail1417 <seminma@clubpixel.ru>
2023-01-28 16:04:18 +0300

    Прочая информация

---
+++ src/Lessons/Lesson3.java
@@ -0,0 +1,14 @@
+package Lessons;
+
+import java.util.ArrayList;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList();
+        list.add(2809);
+
+        for (Integer o: list){
+            System.out.println(o);
+        }
+    }
+}
--- src/Lessons/Seminar1.java
+++ src/Lessons/Seminar1.java
@@ -1,3 +1,5 @@
+package Lessons;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
